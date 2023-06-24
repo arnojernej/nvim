26084910fc3e6698f6f25ba830fadb6f5ec441f2
Arno Jernej <arno@jernej@gmail.com>
2023-06-24 23:00:37 +0200

    update

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,7 +2,11 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
    -- Add languages to be installed here that you want installed for treesitter
-   ensure_installed = { 'c', 'cpp', 'lua', 'python', 'tsx', 'typescript', 'vim', 'help',  'javascript' },
+   ensure_installed = { 'c', 'cpp', 'lua', 'python', 'tsx', 'typescript', 'vim', 'javascript' },
+
+   autotag = {
+      enable = true,
+   },
 
    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
    auto_install = false,
