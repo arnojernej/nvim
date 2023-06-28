bc96d7d65632acbf0f3b8b4e405342a11d9efa75
Arno Jernej <arno.jernej@mahle.com>
2023-06-28 15:28:58 +0200

    update

--- after/plugin/yankhighlight.lua
+++ after/plugin/yankhighlight.lua
@@ -1,12 +1,10 @@
-if not vim.g.vscode then
-   -- [[ Highlight on yank ]]
-   -- See `:help vim.highlight.on_yank()`
-   local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-   vim.api.nvim_create_autocmd('TextYankPost', {
-      callback = function()
-         vim.highlight.on_yank()
-      end,
-      group = highlight_group,
-      pattern = '*',
-   })
-end
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+   callback = function()
+      vim.highlight.on_yank()
+   end,
+   group = highlight_group,
+   pattern = '*',
+})
