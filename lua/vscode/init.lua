e01156a8ae5c2f2dd789828e04cfc836ab187e5b
Arno Jernej <arno.jernej@mahle.com>
2023-06-28 15:26:51 +0200

    update

--- lua/vscode/init.lua
+++ lua/vscode/init.lua
@@ -1,26 +1,29 @@
-require("nvim.lazy")
-require("nvim.map")
-require("nvim.set")
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
--- local augroup = vim.api.nvim_create_augroup
--- -- local ThePrimeagenGroup = augroup('ThePrimeagen', {})
---
--- local autocmd = vim.api.nvim_create_autocmd
--- local yank_group = augroup('HighlightYank', {})
---
--- autocmd('TextYankPost', {
---     group = yank_group,
---     pattern = '*',
---     callback = function()
---         vim.highlight.on_yank({
---             higroup = 'IncSearch',
---             timeout = 150,
---         })
---     end,
--- })
+require("vscode.lazy")
+require("vscode.map")
+require("vscode.set")
 
--- autocmd({"BufWritePre"}, {
---     -- group = ThePrimeagenGroup,
---     pattern = "*",
---     command = [[%s/\s\+$//e]],
--- })
+local augroup = vim.api.nvim_create_augroup
+-- local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 150,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    -- group = ThePrimeagenGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
--- lua/vscode/map.lua
+++ lua/vscode/map.lua
@@ -1,6 +1,3 @@
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
-
 vim.cmd([[
 
 nmap <enter> :w<cr>
