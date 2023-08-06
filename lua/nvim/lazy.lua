fe527136c61a6cc5ca2bf4eed173281267993f4d
Arno Jernej <arno@jernej@gmail.com>
2023-08-06 22:14:07 +0200

    null-ls for prettier

--- lua/nvim/lazy.lua
+++ lua/nvim/lazy.lua
@@ -195,6 +195,11 @@ require('lazy').setup({
       },
    } },
 
+   {
+      'jose-elias-alvarez/null-ls.nvim',
+      dependencies = { "nvim-lua/plenary.nvim" },
+   }
+
    -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
    --       These are some example plugins that I've included in the kickstart repository.
    --       Uncomment any of the lines below to enable them.
