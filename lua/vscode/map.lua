baf00eed0fee1b8b35f7bd3bfd265cfde22f4336
Arno Jernej <arno@jernej@gmail.com>
2023-10-07 00:29:01 +0200

    update

--- lua/vscode/map.lua
+++ lua/vscode/map.lua
@@ -8,6 +8,7 @@ nnoremap <silent> K <Cmd>call VSCodeNotify('workbench.action.closeActiveEditor')
 
 nnoremap <silent> <leader>r <Cmd>call VSCodeNotify('workbench.action.openRecent')<CR>
 nnoremap <silent> <leader>p <Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>
+nnoremap <silent> <leader>e <Cmd>call VSCodeNotify('workbench.view.explorer')<CR>
 
 xmap <silent> <tab> <Cmd>call VSCodeCall('editor.action.indentLines')<CR>
 xmap <silent> <s-tab> <Cmd>call VSCodeCall('editor.action.outdentLines')<CR>
@@ -17,13 +18,13 @@ xmap <silent> <leader>/ <Cmd>call VSCodeCall('editor.action.commentLine')<CR>
 
 nnoremap <silent> ? <Cmd>call VSCodeNotify('workbench.action.findInFiles', { 'query': expand('<cword>') })<CR><Cmd>call VSCodeNotify('workbench.action.openEditorAtIndex1')0<CR>
 
-nmap <silent> gn <Cmd>call VSCodeCall('search.action.focusNextSearchResult')<CR>
+nmap <silent> gn <Cmd>call VSCodeCall('search.action.focusNextSearchResult')<CR><cmd>call VSCodeNotify('cancelSelection')<CR>
 nmap <silent> gN <Cmd>call VSCodeCall('search.action.focusPreviousSearchResult')<CR>
 
 nmap <silent> <space> <Cmd>call VSCodeNotify('editor.toggleFold')<CR>
 nmap <silent> <space><space> <Cmd>call VSCodeNotify('editor.toggleFold')<CR>
 
-nmap <silent> R <Cmd>call VSCodeCall('editor.action.focusNextSearchResult')<CR><Cmd>call VSCodeCall('vscode-neovim.escape')<CR>
+" nmap <silent> R <Cmd>call VSCodeCall('editor.action.focusNextSearchResult')<CR><Cmd>call VSCodeCall('vscode-neolua vim.escape')<CR>
 
 " nnoremap j :call VSCodeNotify('cursorDown')<cr>
 " nnoremap k :call VSCodeNotify('cursorUp')<cr>
