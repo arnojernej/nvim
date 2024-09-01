f2b35dba9cf29c0ddb903bfe174650220390243e
Arno Jernej <arno@jernej@gmail.com>
2024-09-01 19:12:09 +0200

    expand selection with v/V

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -13,10 +13,8 @@ if not vim.g.vscode then
       incremental_selection = {
          enable = true,
          keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<M-space>',
+            node_incremental = "v",
+            node_decremental = "V",
          },
       },
       textobjects = {
