0c657d17a41ccad0201231e61f8c6323f3923f99
Arno Jernej <arno.jernej@mahle.com>
2025-02-28 19:21:26 +0100

    lua stylua

--- lua/nvim/plugins/recent.lua
+++ lua/nvim/plugins/recent.lua
@@ -1,24 +1,23 @@
 return {
 
-   -- {
-   --    'prochri/telescope-all-recent.nvim',
-   --    dependencies = {
-   --       "nvim-telescope/telescope.nvim",
-   --       "kkharji/sqlite.lua",
-   --    },
-   --    opts = { }
-   -- },
+	-- {
+	--    'prochri/telescope-all-recent.nvim',
+	--    dependencies = {
+	--       "nvim-telescope/telescope.nvim",
+	--       "kkharji/sqlite.lua",
+	--    },
+	--    opts = { }
+	-- },
 
-   -- {
-   --    "nvim-telescope/telescope-frecency.nvim",
-   --    config = function()
-   --       require("telescope").load_extension "frecency"
-   --    end,
-   --    opts = {
-   --       workspaces = {
-   --          ["dev"] = "~/dev",
-   --       }
-   --    }
-   -- },
-
+	-- {
+	--    "nvim-telescope/telescope-frecency.nvim",
+	--    config = function()
+	--       require("telescope").load_extension "frecency"
+	--    end,
+	--    opts = {
+	--       workspaces = {
+	--          ["dev"] = "~/dev",
+	--       }
+	--    }
+	-- },
 }
