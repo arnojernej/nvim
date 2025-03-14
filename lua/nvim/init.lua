0336d16069438890d85c6093f848265c67e6cbcc
Arno Jernej <arno.jernej@mahle.com>
2025-03-14 20:40:18 +0100

    update border

--- lua/nvim/init.lua
+++ lua/nvim/init.lua
@@ -8,3 +8,15 @@ require 'nvim.lazy_init'
 -- require 'nvim.snips'
 
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, { pattern = '*', command = [[%s/\s\+$//e]] })
+
+vim.diagnostic.config {
+   -- update_in_insert = true,
+   float = {
+      focusable = false,
+      style = 'minimal',
+      border = 'rounded',
+      source = 'always',
+      header = '',
+      prefix = '',
+   },
+}
