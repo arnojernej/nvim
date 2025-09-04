99e527f7cf5a9be8c5dbe3a52e7b118577e156bb
Arno Jernej <arno.jernej@mahle.com>
2025-09-04 16:41:47 +0200

    added dbt runner

--- ftplugin/sql.lua
+++ ftplugin/sql.lua
@@ -1,1 +1,12 @@
-vim.keymap.set("n", "<leader>x", ":w<cr>:cd .<cr>:term dbt run --select %<cr>", { silent = false })
+vim.keymap.set("n", "<leader>x", function()
+    vim.cmd('w')
+    local dbt_project_root = vim.fn.findfile('dbt_project.yml', '.;')
+    if dbt_project_root ~= '' then
+        dbt_project_root = vim.fn.fnamemodify(dbt_project_root, ':h')
+    else
+        dbt_project_root = vim.loop.cwd()
+    end
+    local model_name = vim.fn.expand('%:t:r')  -- Get filename without extension
+    vim.cmd('cd ' .. dbt_project_root)
+    vim.cmd('term dbt run --select ' .. model_name)
+end, { silent = false })
